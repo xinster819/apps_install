d2f6e9bd08cc336f6443d53fb1394af4ca781311
alchemistli <alchemistli@sohu-inc.com>
2015-11-03 17:13:34 +0800

    add resin deploy

---
+++ resin/resin-4.0.46_global_install.sh
@@ -0,0 +1,26 @@
+resin="resin-4.0.46"
+resin_tar="$resin.tar.gz"
+sources="/opt/sources/"
+apps_intall="/opt/apps_intall/"
+
+if [[ ! -d "$apps_install$resin" ]] && [[ ! -d "$sources$resin_tar" ]]; then
+mkdir -p $source
+cd $sources
+wget -d /opt/sources/ http://caucho.com/download/resin-4.0.46.tar.gz
+tar -zxvf $resin_tar
+fi
+
+if [ ! -d "$apps_install$resin" ]; then
+mkdir -p /opt/apps_install
+mkdir -p /opt
+cd $sources$resin
+./configure --prefix=/opt/apps_install/$resin
+make && make install
+ln -snf $apps_install/$resin /opt/apps/resin
+fi
+
+mkdir -p /opt/conf/resin
+mkdir -p /opt/logs/resin
+useradd resin
+chown -R resin:resin $apps_install$resin
+echo "all done"
